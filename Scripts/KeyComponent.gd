87bbf73c194e093792788d17f891f8210049e069
Scott Ford <zombeescott@gmail.com>
2024-03-10 22:42:40 -0700

    Can now change octave with mouse (keys are imperfect)

--- Scripts/KeyComponent.gd
+++ Scripts/KeyComponent.gd
@@ -1,5 +1,6 @@
 extends Button
 signal key_pressed
+signal octave_changed
 
 var key : String
 @export var octave : int
@@ -8,15 +9,13 @@ var key : String
 func _ready():
 	# Lets the Keyboard connect to all buttons simply
 	# key_pressed.connect(owner.button_pressed.bind())
+	octave_changed.connect(owner.octave_changed.bind())
 	
 	# Bad Programming:
 	# Grabs the name of the Button node and then stores that.
 	# In _process(), it checks if it is a valid input.
 	# So both the input map and names of the buttons have to follow the same format.
-	# I did this because before I manually inputted all the notes in the inspector.
-	# So I had to change all of them back and found this was easier.
 	key = self.name
-	# print(note)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -27,18 +26,25 @@ func _process(delta):
 
 func _on_button_down():
 	var tempStr : String
-	# key_pressed.emit(note, octave)
-	#octave += 1
 	
 	# Calculates the note from the key_pressed
 	if key.contains("Whole"):
-		tempStr = key.left(1) + str(octave+1)
+		tempStr = key.left(1) + str(octave)
 		NotePlayer.play_note(tempStr, octave)
+		
 	elif key.contains("Flat"):
-		tempStr = key.left(1) + "b" +  str(octave+1)
-		# Sends note played
+		tempStr = key.left(1) + "b" +  str(octave)
 		NotePlayer.play_note(tempStr, octave)
+		
+	elif key.contains("Oct"): # Change the octave
+		if key.contains("Up"):
+			octave_changed.emit(1)
+		elif  key.contains("Down"):
+			octave_changed.emit(-1)
+		
 	else:
-		# Do nothing or play something that isn't implemented
-		pass
+		print("Unknown button pressed")
 	
+
+func change_octave(num : int):
+	octave += num;
--- Scripts/Keyboard.gd
+++ Scripts/Keyboard.gd
@@ -1,5 +1,9 @@
 extends Node
 
+# MAX and MIN octaves are the values that the leftmost C key can play
+@export var MIN_OCTAVE : int = 1
+@export var MAX_OCTAVE : int = 2
+@export var curr_octave : int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,3 +18,22 @@ func _process(delta):
 func button_pressed(note : String, octave : int):
 	# print("Note: ", note, " Octave: ", octave)
 	pass
+
+
+func octave_changed(num : int):
+	if num == 1 and MAX_OCTAVE > curr_octave:
+		# Increase Octave
+		curr_octave += num
+		print("Increased Octave to: " + str(curr_octave))
+		get_tree().call_group("keys", "change_octave", num)
+		print(num)
+		
+	elif num == -1 and MIN_OCTAVE < curr_octave:
+		# Decrease Octave
+		curr_octave += num
+		print("Decreased Octave to: " + str(curr_octave))
+		get_tree().call_group("keys", "change_octave", num)
+		
+	else:
+		print("Couldn't change octave")
+
